9874129a64afa60bb108f96a614c92270597b003
Harry Moreno <morenoh149@gmail.com>
2019-04-11 18:51:35 -0400

    Move hello world into section dir
    * don't think I can name android projects 01-1 or 01_1 so we are using a
    dir to hold the first project

--- 01-1/HelloWorld/app/src/main/java/com/harrymoreno/helloworld/MainActivity.java
+++ 01-1/HelloWorld/app/src/main/java/com/harrymoreno/helloworld/MainActivity.java
@@ -2,12 +2,21 @@ package com.harrymoreno.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "Hello World");
+        String[] myStrings = {"a", "b", "c"};
+        try {
+            Log.i(LOG_TAG, myStrings[10]);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
     }
 }
